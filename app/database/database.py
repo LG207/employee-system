a6d956039703aab11c4497b241d881a17e59a913
Luiz Gustavo Leal Gimenez <luizlg207@gmail.com>
2026-03-22 19:06:04 -0300

    Inserção de PostgreSQL

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./employees.db"
+DATABASE_URL = "postgresql://employee_db_21cv_user:0MRiollSNYGUKff3FmjKST6wf1x1m85A@dpg-d7065hv5gffc73dmko20-a/employee_db_21cv"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
