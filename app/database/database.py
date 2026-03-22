71e570329d2f7b574d9e55116da6477519240421
Luiz Gustavo Leal Gimenez <luizlg207@gmail.com>
2026-03-22 20:41:00 -0300

    Fix Postgre connection 0.3

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://employee_db_lrp3_user:7sPNoDibxTvd52XEAOnJB56OA99PyyCC@dpg-d7075fmuk2gs73955fog-a/employee_db_lrp3")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
