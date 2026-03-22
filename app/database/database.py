76a6ab036d67d5c7e7891c74e64e8881909fc9bd
Luiz Gustavo Leal Gimenez <luizlg207@gmail.com>
2026-03-22 19:43:25 -0300

    Fix postgreSQL connection 0.1

--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = "postgresql://employee_db_21cv_user:0MRiollSNYGUKff3FmjKST6wf1x1m85A@dpg-d7065hv5gffc73dmko20-a/employee_db_21cv"
+SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://employee_db_21cv_user:0MRiollSNYGUKff3FmjKST6wf1x1m85A@dpg-d7065hv5gffc73dmko20-a/employee_db_21cv")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
